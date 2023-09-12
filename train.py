b342e0fa465d1dd46425ceb57eca14d3ebb60ee5
ishnoor-singh2 <48507803+ishnoor-singh2@users.noreply.github.com>
2023-09-12 23:48:22 +0530

    Update train.py

--- train.py
+++ train.py
@@ -207,10 +207,11 @@ def train(labeled_trainloader, unlabeled_trainloader, model, optimizer, ema_opti
 
 
         try:
-            (inputs_u, inputs_u2), _ = unlabeled_train_iter.next()
+            (inputs_u, inputs_u2), _ = next(unlabeled_train_iter)
         except:
             unlabeled_train_iter = iter(unlabeled_trainloader)
-            (inputs_u, inputs_u2), _ = unlabeled_train_iter.next()
+            (inputs_u, inputs_u2), _ = next(unlabeled_train_iter)
+
 
         # measure data loading time
         data_time.update(time.time() - end)
