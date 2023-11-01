6684114e7761bcd7b1a5c815789edcbd53e97898
Evening <johnchangqi@hotmail.com>
2023-11-01 19:26:28 +0800

    Removed unused 'get_mean_and_std' function in utils/misc.py

--- utils/misc.py
+++ utils/misc.py
@@ -1,5 +1,4 @@
 """Some helper functions for PyTorch, including:
-    - get_mean_and_std: calculate the mean and std value of dataset.
     - msr_init: net parameter initialization.
     - progress_bar: progress bar mimic xlua.progress.
 """
@@ -9,25 +8,7 @@ import os
 import torch.nn as nn
 import torch.nn.init as init
 
-__all__ = ["get_mean_and_std", "init_params", "mkdir_p", "AverageMeter"]
-
-
-def get_mean_and_std(dataset):
-    """Compute the mean and std value of dataset."""
-    dataloader = trainloader = torch.utils.data.DataLoader(
-        dataset, batch_size=1, shuffle=True, num_workers=2
-    )
-
-    mean = torch.zeros(3)
-    std = torch.zeros(3)
-    print("==> Computing mean and std..")
-    for inputs, targets in dataloader:
-        for i in range(3):
-            mean[i] += inputs[:, i, :, :].mean()
-            std[i] += inputs[:, i, :, :].std()
-    mean.div_(len(dataset))
-    std.div_(len(dataset))
-    return mean, std
+__all__ = ["init_params", "mkdir_p", "AverageMeter"]
 
 
 def init_params(net):
