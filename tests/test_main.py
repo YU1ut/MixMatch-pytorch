3783ba75e077cf12dd2cb98edc97ee29300c6361
Eve-ning <dev_evening@hotmail.com>
2023-11-01 21:00:19 +0800

    Add fixed constant for test

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,7 +7,9 @@ def test_main_seed():
     Ensure that training is always the same when seeded."""
     epochs = 1
     best_acc_1, mean_acc_1 = main(epochs=epochs)
-    best_acc_2, mean_acc_2 = main(epochs=epochs)
+    # best_acc_2, mean_acc_2 = main(epochs=epochs)
 
-    assert best_acc_1 == best_acc_2
-    assert mean_acc_1 == mean_acc_2
+    # assert best_acc_1 == best_acc_2
+    # assert mean_acc_1 == mean_acc_2
+    assert best_acc_1 == 20.28
+    assert mean_acc_1 == 19.54
