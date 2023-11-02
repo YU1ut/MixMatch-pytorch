9ca94fa147eaf278fd04b67850bc55e5e9799067
Eve-ning <dev_evening@hotmail.com>
2023-11-02 23:28:43 +0800

    Update training metrics for tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,13 +9,13 @@ def test_main_seed_fast():
         epochs=epochs, train_iteration=train_iteration
     )
 
-    assert best_acc_1 == 7.98
-    assert mean_acc_1 == 8.17
+    assert best_acc_1 == 8.53333332570394
+    assert mean_acc_1 == 8.79
 
 
 def test_main_seed_epoch():
     """Ensure that the model doesn't change when refactoring"""
     epochs = 1
     best_acc_1, mean_acc_1 = main(epochs=epochs)
-    assert best_acc_1 == 22.58
-    assert mean_acc_1 == 22.15
+    assert best_acc_1 == 24.266666646321614
+    assert mean_acc_1 == 23.01
