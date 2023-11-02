d442101951f8e8c48f999efaa6e53122a8edb273
Eve-ning <dev_evening@hotmail.com>
2023-11-02 16:56:11 +0800

    Update datasetload to use new signature

--- tests/test_dataset_load.py
+++ tests/test_dataset_load.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from mixmatch.dataset.cifar10 import get_cifar10
+from mixmatch.dataset.cifar10 import get_dataloaders
 
 
 def test_load_seeded():
@@ -10,32 +10,42 @@ def test_load_seeded():
     seed = 42
 
     (
-        labeled_trainloader,
-        unlabeled_trainloader,
+        train_lbl_dl,
+        train_unl_dl,
         val_loader,
         test_loader,
-    ) = get_cifar10("./data", n_labeled, batch_size=batch_size, seed=seed)
-
-    labeled_targets_1 = labeled_trainloader.dataset.targets
-    unlabeled_targets_1 = unlabeled_trainloader.dataset.targets
-    val_targets_1 = val_loader.dataset.targets
+        classes,
+    ) = get_dataloaders(
+        dataset_dir="./data",
+        train_lbl_size=0.005,
+        train_unl_size=0.980,
+        batch_size=batch_size,
+        seed=seed,
+    )
+    labeled_indices_1 = train_lbl_dl.dataset.indices
+    unlabeled_indices_1 = train_unl_dl.dataset.indices
+    val_indices_1 = val_loader.dataset.indices
     test_targets_1 = test_loader.dataset.targets
 
     (
-        labeled_trainloader,
-        unlabeled_trainloader,
+        train_lbl_dl,
+        train_unl_dl,
         val_loader,
         test_loader,
-    ) = get_cifar10("./data", n_labeled, batch_size=batch_size, seed=seed)
-
-    labeled_targets_2 = labeled_trainloader.dataset.targets
-    unlabeled_targets_2 = unlabeled_trainloader.dataset.targets
-    val_targets_2 = val_loader.dataset.targets
+        classes,
+    ) = get_dataloaders(
+        dataset_dir="./data",
+        train_lbl_size=0.005,
+        train_unl_size=0.980,
+        batch_size=batch_size,
+        seed=seed,
+    )
+    labeled_indices_2 = train_lbl_dl.dataset.indices
+    unlabeled_indices_2 = train_unl_dl.dataset.indices
+    val_indices_2 = val_loader.dataset.indices
     test_targets_2 = test_loader.dataset.targets
 
-    assert np.all(labeled_targets_1 == labeled_targets_2)
-    assert np.all(unlabeled_targets_1 == unlabeled_targets_2)
-    assert np.all(val_targets_1 == val_targets_2)
+    assert np.all(labeled_indices_1 == labeled_indices_2)
+    assert np.all(unlabeled_indices_1 == unlabeled_indices_2)
+    assert np.all(val_indices_1 == val_indices_2)
     assert np.all(test_targets_1 == test_targets_2)
-
-    print(labeled_targets_1)
