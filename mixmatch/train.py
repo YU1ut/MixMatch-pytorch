3b0e1ff4b0860ca6c84d6c3a1b00ca6d2f8608fe
Eve-ning <dev_evening@hotmail.com>
2023-11-02 13:48:45 +0800

    Remove unused save_checkpoint

--- mixmatch/train.py
+++ mixmatch/train.py
@@ -47,20 +47,6 @@ def validate(
     )
 
 
-def save_checkpoint(
-    state,
-    is_best: bool,
-    checkpoint: str,
-    filename: str = "checkpoint.pth.tar",
-):
-    filepath = os.path.join(checkpoint, filename)
-    torch.save(state, filepath)
-    if is_best:
-        shutil.copyfile(
-            filepath, os.path.join(checkpoint, "model_best.pth.tar")
-        )
-
-
 def linear_rampup(current: float, rampup_length: int):
     if rampup_length == 0:
         return 1.0
